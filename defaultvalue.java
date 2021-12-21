c0a02a2752fdd844c0f7b596408e7b3ccd86ab5c
AkshatRana <akshat.xworkz@gmail.com>
2021-12-21 07:57:08 +0530

    default value is added

---
+++ defaultvalue.java
@@ -0,0 +1,14 @@
+class DefaultValue{
+		public static void main (String[] args){
+		System.out.println(age);
+		System.out.println(sh);
+		System.out.println(in);
+		System.out.println(ln);
+		System.out.println(fl);
+		System.out.println(db);
+		System.out.println(bl);
+		System.out.println(ch);		
+		}
+		static byte age;
+		static short 
+}
